5d411ba4c81f44a239e855c1e8d4fba6f6cd744c
Ted "skjsjhb" Gao <skjsjhb@outlook.com>
2024-03-11 15:59:42 +0800

    Fix checksum calculation
    - Fix missing leading zeros when using BigInteger.
    - Alter the use of RandomAccessFile due to JDK bugs.

--- src/main/kotlin/skjsjhb/mc/hyaci/util/Files.kt
+++ src/main/kotlin/skjsjhb/mc/hyaci/util/Files.kt
@@ -1,9 +1,9 @@
 package skjsjhb.mc.hyaci.util
 
-import java.io.RandomAccessFile
-import java.math.BigInteger
-import java.nio.channels.FileChannel
+import java.io.FileInputStream
 import java.security.MessageDigest
+
+private const val bufferSize = 512
 
 /**
  * Calculates the checksum of the given file.
@@ -11,8 +11,21 @@ import java.security.MessageDigest
  * The resulted hash string is in lowercase, encoded as HEX.
  */
 fun checksumOf(f: String, algo: String): String =
-    RandomAccessFile(f, "r").use {
-        it.channel.map(FileChannel.MapMode.READ_ONLY, 0, it.channel.size()).let {
-            MessageDigest.getInstance(algo).apply { update(it) }.digest()
+    MessageDigest.getInstance(algo).apply {
+        FileInputStream(f).use {
+            val bytes = ByteArray(bufferSize)
+            while (true) {
+                val bytesRead = it.read(bytes)
+                if (bytesRead == -1) {
+                    break
+                }
+                update(bytes, 0, bytesRead)
+            }
         }
-    }.let { BigInteger(1, it).toString(16) }.lowercase()
+    }.digest().let {
+        StringBuilder().apply {
+            it.forEach { append(String.format("%02x", it)) }
+        }.toString().lowercase()
+    }
+
+
