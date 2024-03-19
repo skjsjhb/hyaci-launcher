b86fc3e869f6f2aaea72094d248926d25b044159
Ted "skjsjhb" Gao <skjsjhb@outlook.com>
2024-03-19 16:16:37 +0800

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("plugin.serialization") version "1.9.23"
     id("org.jetbrains.dokka") version "1.9.20"
     id("idea")
     id("application")
@@ -22,13 +22,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-core:2.22.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
     implementation("org.apache.logging.log4j:log4j-api:2.23.1")
     implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.23.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("com.h2database:h2:2.2.224")
     implementation("me.friwi:jcefmaven:122.1.10")
-    implementation("com.github.jponge:lzma-java:1.2")
+    implementation("com.github.jponge:lzma-java:1.3")
     implementation("com.microsoft:credential-secure-storage:1.0.0")
 
     runtimeOnly("org.fusesource.jansi:jansi:1.18")
